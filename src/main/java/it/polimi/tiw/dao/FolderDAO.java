648c80087ee9e01d965ea30a5d928dff9f20d2ce
GioBar00 <38290406+GioBar00@users.noreply.github.com>
2022-06-08 21:43:55 +0200

    added documentation

--- src/main/java/it/polimi/tiw/dao/FolderDAO.java
+++ src/main/java/it/polimi/tiw/dao/FolderDAO.java
@@ -47,61 +47,6 @@ public class FolderDAO {
     }
 
     /**
-     * This method returns the {@link Folder} with the given id.
-     *
-     * @param id the id of the folder.
-     * @return the {@link Folder} with the given id, null if it does not exist.
-     * @throws SQLException if an error occurs during the query.
-     */
-    public Folder getFolder(int id) throws SQLException {
-        String query = "SELECT idfolder, name, creationDate, user_iduser FROM folder WHERE idfolder = ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setInt(1, id);
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                return new Folder(resultSet.getInt("idfolder"), resultSet.getString("name"),
-                        resultSet.getDate("creationDate"), resultSet.getInt("user_iduser"));
-            }
-        }
-        return null;
-    }
-
-    /**
-     * This method returns a map of the {@link Folder}s of a specified user with the corresponding {@link SubFolder}s.
-     *
-     * @param ownerId the id of the owner.
-     * @return a map of the {@link Folder}s of a specified user with the corresponding {@link SubFolder}s.
-     * @throws SQLException if an error occurs during the query.
-     */
-    public Map<Folder, List<SubFolder>> getFoldersWithSubFolders(int ownerId) throws SQLException {
-        String query = "SELECT * FROM folder f LEFT JOIN subfolder s on f.idfolder = s.folder_idfolder WHERE f.user_iduser = ?";
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setInt(1, ownerId);
-            ResultSet resultSet = statement.executeQuery();
-            Map<Folder, List<SubFolder>> folders = new HashMap<>();
-            while (resultSet.next()) {
-                int idfolder = resultSet.getInt("idfolder");
-                Folder folder;
-                Optional<Folder> optionalFolder = folders.keySet().stream().filter(f -> f.id() == idfolder).findFirst();
-                if (optionalFolder.isPresent()) {
-                    folder = optionalFolder.get();
-                } else {
-                    folder = new Folder(idfolder, resultSet.getString("f.name"),
-                            resultSet.getDate("f.creationDate"), resultSet.getInt("user_iduser"));
-                    folders.put(folder, new LinkedList<>());
-                }
-                if (resultSet.getString("s.name") != null) {
-                    SubFolder subFolder = new SubFolder(resultSet.getInt("idsubfolder"), resultSet.getString("s.name"),
-                            resultSet.getDate("s.creationDate"),
-                            resultSet.getInt("folder_idfolder"));
-                    folders.get(folder).add(subFolder);
-                }
-            }
-            return folders;
-        }
-    }
-
-    /**
      * This method returns a map of the {@link Folder}s of a specified user with the corresponding {@link SubFolder}s and {@link Document}s.
      *
      * @param ownerId the id of the owner.
